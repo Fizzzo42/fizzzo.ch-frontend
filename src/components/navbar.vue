a5c8a4e5d2512c879d0cafd8fc00e9b0f3ab1058
Özhan Kaya <Fizzzo42@gmail.com>
2018-11-24 00:17:41 +0100

    added home page

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -8,15 +8,12 @@
     <b-collapse is-nav id="nav_collapse">
 
       <b-navbar-nav>
-        <b-nav-item href="#">Link 1</b-nav-item>
-        <b-nav-item href="#">Link 2</b-nav-item>
-        <b-nav-item href="#">Link 3</b-nav-item>
         <b-nav-item :to="{ name: 'serverStatus' }">Server Status</b-nav-item>
       </b-navbar-nav>
 
       <!-- Right aligned nav items -->
       <b-navbar-nav class="ml-auto">
-        <b-nav-text>Add something useful here ;)</b-nav-text>
+        <b-nav-text>Random Text</b-nav-text>
       </b-navbar-nav>
 
     </b-collapse>
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import ServerStatus from '../views/serverStatus'
+import Home from '../views/home'
 
 Vue.use(VueRouter);
 
 export default new VueRouter({
   routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
     {
       path: '/serverStatus',
       name: 'serverStatus',
